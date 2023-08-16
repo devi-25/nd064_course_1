c9d1c5f43ad7b16a8f8e17a8e131c86f7d7282a8
Devi-Babusundaram <devi.babusundaram@coxautoinc.com>
2023-08-16 08:43:22 -0400

    First commit

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,36 @@
+import logging
 from flask import Flask
+from flask import json
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("Main Request Successfull")
     return "Hello World!"
 
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Status response returned")
+    app.logger.debug(response)
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Metrics response returned")   
+    return response    
+
 if __name__ == "__main__":
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
     app.run(host='0.0.0.0')
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
@@ -22,13 +23,42 @@ def get_post(post_id):
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+
+
 # Define the main route of the web application 
 @app.route('/')
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    index.counter += 1
     return render_template('index.html', posts=posts)
+index.counter = 0
+
+@app.route('/healthz')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    #app.logger.info("Status response returned")
+    #app.logger.debug(response)
+    return response
+
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post  = connection.execute('SELECT COUNT(*) FROM posts').fetchone()
+    count = post[0]
+    connection.close()
+    response = app.response_class(            
+            response=json.dumps({"status":"success","code":200,"data":{"db_connection_count": index.counter , "post_count": count}}),
+            status=200,
+            mimetype='application/json'
+    )
+    #app.logger.info("Metrics response returned")   
+    return response  
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
@@ -36,13 +66,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('404 - Non existing article is accessed')
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article '+ post['title'] + ' retrived')
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About us page is accessed')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,11 +93,12 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('New Article '+title+' is created')
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
 # start the application on port 3111
 if __name__ == "__main__":
+   logging.basicConfig(level=logging.INFO, style='{', datefmt='%Y-%m-%d %H:%M:%S', format='{levelname}:{name}:{asctime} {message}')
    app.run(host='0.0.0.0', port='3111')
